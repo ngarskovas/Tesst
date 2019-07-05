66521efb28040f679981190a2c2c9b7b4965461a
ngarskovas <ngarskovas@gmail.com>
2019-07-05 07:40:49 -0400

    .addEventListener

--- splah.js
+++ splah.js
@@ -4,8 +4,10 @@ var video = document.getElementById("myVideo");
 // Get the button
 var btn = document.getElementById("myBtn");
 
+
+
 // Pause and play the video, and change the button text
-function myFunction() {
+btn.addEventListener('click', myFunction() {
   if (video.paused) {
     video.play();
     btn.innerHTML = "Pause";
